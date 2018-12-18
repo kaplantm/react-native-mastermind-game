c4b149e508aa98f392a8418a777169a3eb1d8876
Toni Kaplan <tonarie.k@gmail.com>
2018-12-18 15:57:55 -0500

    update packages and made necessary changes to react navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import React from "react";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import MainTabNavigator from "./MainTabNavigator";
 
-import MainTabNavigator from './MainTabNavigator';
-
-export default createSwitchNavigator({
+const AppNavigator = createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  Main: {
+    screen: MainTabNavigator
+  }
 });
+
+export default createAppContainer(AppNavigator);
